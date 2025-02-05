33e4b1d3f05a8e3758aed095954a9431956d8ab6
Magnus James Terry <mterry17@hydra14.eecs.utk.edu>
2025-02-05 12:10:58 -0500

    list

--- proj2/list.cpp
+++ proj2/list.cpp
@@ -2,14 +2,35 @@
 
 
 List::List() {
-
-
+    head = nullptr;
 }
 
 List::~List() {
-
+    Node* node = head;
+    while(node != nullptr){
+        Node* temp = node;
+        node = node->next;
+        delete temp;
+    }
 }
 
 void List::push_front(const std::string &s) {
- 
+    // Make a new node
+    // Place in front of head node
+    // stoi the value - if fail, default to 0
+
+    int value;
+    for(size_t i = 0; i < s.size(); i++){
+        if(!isdigit(s[i])){
+            value = stoi(s);
+        }
+        else{
+            value = 0;
+        }
+    }
+
+    Node* newNode = new Node();
+    newNode->number = value;
+    newNode->next = head;
+    head = newNode;
 }
