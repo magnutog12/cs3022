58cd9a7c957027aff7a8c65df1da3ba23c164e74
Ada Jane Whitefield <awhitef3@hydra22.eecs.utk.edu>
2025-02-09 18:37:58 -0500

    qsort progress

--- proj2/qsort.cpp
+++ proj2/qsort.cpp
@@ -2,9 +2,13 @@
 
 #include "volsort.h"
 
+#include <iostream>
 #include <cstdlib>
+#include <cstring>
 #include <array>
 #include <iostream>
+
+using namespace std;
 
 int int_cmp(const void *a, const void *b){
     const int *ia = (const int *) a;
@@ -12,19 +16,42 @@ int int_cmp(const void *a, const void *b){
     return *ia - *ib;
 }
 
-void qsort_sort(List &l, bool numeric) {
-    //Choose pivot in list --> should be recursive
-    Node* array[l.size()];
+int cstring_cmp(const void *a, const void *b){
+    const char **ia = (const char **)a;
+    const char **ib = (const char **)b;
+    return strcmp(*ia, *ib);
+}
 
-    if(numeric){
-        
+void qsort_sort(List &l, bool numeric) {
+    Node* top = l.head;
+    int count = 0;
+    
+
+    while(top){
+        count++;
+        top = top->next;
+    }
+    
+    Node* array[count];
+
+    top = l.head;
+    int index = 0;
+    while(top){
+        array[index] = top;
+        top = top->next;
+        index++;
+    }
+
+    if(numeric == true){
+        qsort(array, count, sizeof(Node*), int_cmp);
     }
     else{
-
+        qsort(array, count, sizeof(Node*), cstring_cmp);
     }
 
-    Node* pivot = l.head;
-
+    for(int i = 0; i < count; i++){
+        array[i]->next = array[i+1];
+    }
 
 }
 
